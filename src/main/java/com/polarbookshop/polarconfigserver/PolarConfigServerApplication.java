2dea949972fcf977dd90b812ab5ff4b772da7ea9
haebyun <bhk4392@gmail.com>
2024-09-13 15:41:57 +0900

    init

---
+++ src/main/java/com/polarbookshop/polarconfigserver/PolarConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.polarconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarConfigServerApplication.class, args);
+    }
+
+}
